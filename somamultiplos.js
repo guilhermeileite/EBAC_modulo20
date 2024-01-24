fe44a264cffd350c576cf7c5bd5e5a0ef579238b
guilhermeileite <130417023+guilhermeileite@users.noreply.github.com>
2024-01-24 09:40:51 -0300

    Create somamultiplos.js

---
+++ somamultiplos.js
@@ -0,0 +1,16 @@
+// Criando uma função para fazer a soma dos múltiplos de 5 ou 7 e inicializando em zero a variável "soma" 
+function somaMultiplos() {
+    let soma = 0;
+
+    // Usando o laço "for" para percorrer os números de 1 a 999
+    // Condicionando o índice a ter resto zero na dívisão por 5 ou 7 e incrementando na variável "soma"  
+    for (let i = 1; i < 1000; i++) {
+        if (i % 5 == 0 || i % 7 == 0) {
+            soma = soma + i;
+        }
+    }
+    return soma;
+}
+//Executando o código
+const somaFinal = somaMultiplos();
+console.log(somaFinal);
